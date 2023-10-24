3944a90f89da7d44cffc5ebb71b4428652eb75e8
jakobs <jakob.schmierer@students.frc5113.com>
2023-10-24 19:14:45 +0000

    completed exercise 1 (make lists)

---
+++ chapter3/exercise1.py
@@ -0,0 +1,7 @@
+games = ["reading","video games","killing families of 4","wasting time"]
+
+foods = ["hamburger","cheeseburger","families of 4","pizza"]
+
+favorites = games + foods
+
+print(favorites)
