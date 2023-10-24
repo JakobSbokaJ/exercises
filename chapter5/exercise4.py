e1dde0fb687653c7ad53799475f1502aad344901
jakobs <jakob.schmierer@students.frc5113.com>
2023-10-24 19:46:11 +0000

    Completed exercise 4 (even more if statements)

---
+++ chapter5/exercise4.py
@@ -0,0 +1,10 @@
+import random
+ninjas = random.randint(0,50)
+
+print(ninjas)
+if ninjas < 10:
+    print("I can fight those ninjas!")
+elif ninjas < 30:
+    print("It'll be a struggle, but I can take 'em")
+else:
+    print("That's too many")
