c76c8a62e75ffc71f442cafec3d2ad07132b56f0
jakobs <jakob.schmierer@students.frc5113.com>
2023-10-24 19:28:01 +0000

    completed exercise 4 (multiline strings with f strings)

---
+++ chapter3/exercise4.py
@@ -0,0 +1,13 @@
+center = " " * 25
+print(f"""{center} 12 Butts Wynd
+{center} Twinklebottom Heath
+{center} West Snoring
+
+Dear Sir
+
+I wish to report that tiles are missing from the
+outside toilet roof.
+I think it was bad wind the other night that blew them away.
+
+Regards
+Malcom Dithering""")
