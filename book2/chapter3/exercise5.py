7bb40e3663b535144a42f4f9525ffce0ba1f4355
jakobs <jakob.schmierer@students.frc5113.com>
2023-11-13 19:34:11 +0000

    complete ex 5 ch 3

---
+++ book2/chapter3/exercise5.py
@@ -0,0 +1,11 @@
+gList = ["Eren Bunul","Eren's mom","Eren's dad"]
+
+for guest in gList:
+    print(f"{guest}, please come to my dinner. It'll be quite fire.")
+
+print("Sadly, Eren's mom cannot make it.")
+
+gList = ["Eren Bunul","fedy fabear","Eren's dad"]
+
+for guest in gList:
+    print(f"{guest}, I am begging you to pull up to my dinner party. There will be water.")
