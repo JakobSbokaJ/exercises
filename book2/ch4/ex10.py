69eafad3590e604fea50f255afcb37a2b4721884
jakobs <jakob.schmierer@students.frc5113.com>
2023-11-13 20:27:23 +0000

    complete ex 10

---
+++ book2/ch4/ex10.py
@@ -0,0 +1,8 @@
+nums = []
+
+for i in range(1,1000001):
+    nums.append(i)
+
+print(f"The first 3 items in the list are {nums[:3]}")
+print(f"Three items from the middle of the list are {nums[628124:628127]}")
+print(f"The last 3 items in the list are {nums[999997:]}")
