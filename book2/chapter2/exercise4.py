59322b3871d25f8b76c69b4525a87c6b1c35320c
jakobs <jakob.schmierer@students.frc5113.com>
2023-11-13 19:17:43 +0000

    exercise 4 ch 2

---
+++ book2/chapter2/exercise4.py
@@ -0,0 +1,4 @@
+name = input("Input your name: ")
+print(f"Your name in lowercase is {name.lower()}")
+print(f"Your name in uppercase is {name.upper()}")
+print(f"Your name in titlecase is {name.title()}")
