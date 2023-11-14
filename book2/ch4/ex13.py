44f5390c10bdc9148b1902de2e3ef4b6b5487512
jakobs <jakob.schmierer@students.frc5113.com>
2023-11-14 19:24:56 +0000

    complete ex 13 ch 4

--- book2/ch4/ex13.py
+++ book2/ch4/ex13.py
@@ -3,3 +3,9 @@ buffetFoods = ["chicken", "steak", "turkey", "scallop", "onion"]
 for food in buffetFoods:
     print(f"This restaurant serves {food}.")
 
+buffetFoods[1] = "burger"
+buffetFoods[4] = "macncheese"
+
+print("Menu update!")
+for food in buffetFoods:
+    print(f"Item: {food}")
