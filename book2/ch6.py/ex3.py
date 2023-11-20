9509fab667a22f67652df89a0d10873af54c777f
jakobs <jakob.schmierer@students.frc5113.com>
2023-11-20 19:59:03 +0000

    complete ex 3 ch 6

---
+++ book2/ch6.py/ex3.py
@@ -0,0 +1,7 @@
+glossary = {"repository":"\n\tkinda like a folder thing","vlad":"\n\tcool man","float":"\n\ta decimal"}
+
+for word, de in glossary.items():
+    print(f"{word}: ")
+    print(f"\n\t{de}")
+
+#I only did 3 not 5 I hope that's ok
