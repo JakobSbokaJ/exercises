a7343e4a7c18a3d863441ea8c7b9e87d80cc10bc
jakobs <jakob.schmierer@students.frc5113.com>
2023-11-20 20:07:19 +0000

    complete ex 5 ch 6

---
+++ book2/ch6.py/ex5.py
@@ -0,0 +1,6 @@
+rivers = {"Amazon":"Brazil","Nile":"Egypt","Thames":"England"}
+
+for river, country in rivers.items():
+    print(f"The {river} runs through {country}.")
+    print(f"River: {river}")
+    print(f"Country: {country}")
