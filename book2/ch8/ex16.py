4edf0b9a9e92fc4f170cc5695fa832592bd7f815
jakobs <jakob.schmierer@students.frc5113.com>
2023-11-21 19:45:10 +0000

    complete ex 16 ch 8 - mostly understand

--- book2/ch8/ex16.py
+++ book2/ch8/ex16.py
@@ -1,3 +1,11 @@
 import ex3
 
-ex3.make_shirt("medium","men are men")
+ex3.make_shirt("Medium", "eren HATES the women")
+from ex3 import make_shirt
+make_shirt("Medium", "eren HATES the women!")
+from ex3 import make_shirt as ms
+ms("Medium", "eren HATES the women!!")
+import ex3 as e
+e.make_shirt("Medium", "eren HATES the women!!!")
+from ex3 import *
+make_shirt("Medium", "eren HATES the women!!!!")
