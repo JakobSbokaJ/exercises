ec5de95347e3ec42185c24a52f4cfbe872bc8502
jakobs <jakob.schmierer@students.frc5113.com>
2023-11-28 20:41:39 +0000

    complete ex 10 ch 9 (with help)

--- book2/ch9/ex1.py
+++ book2/ch9/ex1.py
@@ -10,7 +10,9 @@ We serve {self.cuisine_type} cuisine!""")
     def open_restaurant(self):
         print("We are now open!!")
 
-restaurant = Restaurant("m is zesty","lemons")
-print(f"The restaurant name is {restaurant.restaurant_name.title()}, and the cuisine is {restaurant.cuisine_type}.")
-restaurant.describe_restaurant()
-restaurant.open_restaurant()
+
+if __name__ == "__main__":
+    restaurant = Restaurant("m is zesty","lemons")
+    print(f"The restaurant name is {restaurant.restaurant_name.title()}, and the cuisine is {restaurant.cuisine_type}.")
+    restaurant.describe_restaurant()
+    restaurant.open_restaurant()
---
+++ book2/ch9/ex10.py
@@ -0,0 +1,5 @@
+from ex1 import Restaurant
+
+restauran = Restaurant("resporan","foob")
+restauran.describe_restaurant()
+restauran.open_restaurant()
